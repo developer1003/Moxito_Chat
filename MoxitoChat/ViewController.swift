91b70207ef990db630ffa4e7e8f61a20f0e93eb0
Mirsaidov Bekzod <anasxon5746@gmail.com>
2022-12-16 15:02:37 +0500

    Initial Commit

---
+++ MoxitoChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoxitoChat
+//
+//  Created by Mirsaidov Bekzod on 16/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
